d58a99fe9708b7369c67d1a5a83413933b80fe4f
Eshaan <eshetty123@gmail.com>
2024-10-07 14:51:59 -0700

    widget

---
+++ ios/SaplingWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  SaplingWidget
+//
+//  Created by Eshaan Shetty on 9/25/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
